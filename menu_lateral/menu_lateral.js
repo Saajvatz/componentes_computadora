392cdcf621eb4cfeb4759f4080e5db97063de810
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-07 08:45:34 -0600

    v4

---
+++ menu_lateral/menu_lateral.js
@@ -0,0 +1,11 @@
+let div_lateral = document.querySelector("menu-lateral")
+
+div_lateral.innerHTML = 
+    <ul>
+        <li>CPU</li>
+        <li>GPU</li>
+        <li>Disco</li>
+        <li>Unidad</li>
+        <li>RAM</li>
+        <li>Tarjeta Madre</li>
+    </ul> 
