9da6c3493c74d7cc202136bb05f6e38f33c17bf2
pagiron <pagiron@scl.edu.gt>
2024-05-07 09:09:52 -0600

    pablo pausa el trabajo para actualizar el repo

---
+++ menu_lateral/menu_lateral.js
@@ -0,0 +1,12 @@
+let div_lateral=document.querySelector(".menu-lateral")
+
+div_lateral.innerHTML = `
+<ul class="nav">
+    <li>CPU</li>
+    <li>GPU</li>
+    <li>DISCO HDD</li>
+    <li>UNIDAD SSD</li>
+    <li>RAM</li>
+    <li>Trajeta Madre</li>
+
+</ul> `;
