a09c9b07bd3bb6344245c5770a76c36afc70a82a
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-09 10:26:00 -0600

    barra lista

--- menu_lateral/menu_lateral.js
+++ menu_lateral/menu_lateral.js
@@ -1,12 +1,9 @@
 let div_lateral=document.querySelector(".menu-lateral")
 
 div_lateral.innerHTML = `
-<ul>
-    <li>CPU</li>
-    <li>GPU</li>
-    <li>DISCO HDD</li>
-    <li>UNIDAD SSD</li>
-    <li>RAM</li>
-    <li>Trajeta Madre</li>
-
-</ul>`; 
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="CPU.html">CPU</a> </nav> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="GPU.html">GPU</a> </nav> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="TM.html">Tarjeta Madre</a> </nav> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="RAM.html">RAM</a> </nav> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="SSD.html">SSD</a> </nav> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="SIM.html">Tarjeta SIM</a> </nav> </div> </div>`;
