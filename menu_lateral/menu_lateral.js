439e1d142be9696f39560c9700f4ada7d9105bc2
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-15 11:38:30 -0600

    componentes

--- menu_lateral/menu_lateral.js
+++ menu_lateral/menu_lateral.js
@@ -1,7 +1,7 @@
 let div_lateral=document.querySelector(".menu-lateral")
 
 div_lateral.innerHTML = `
-<div class= "cuadros"> <div class= "menus"> <nav> <a href="CPU.html">CPU</a> </nav> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="CPU.html">CPU</a>  </nav> </div> </div> <br>
 <div class= "cuadros"> <div class= "menus"> <nav> <a href="GPU.html">GPU</a> </nav> </div> </div> <br>
 <div class= "cuadros"> <div class= "menus"> <nav> <a href="TM.html">Tarjeta Madre</a> </nav> </div> </div> <br>
 <div class= "cuadros"> <div class= "menus"> <nav> <a href="RAM.html">RAM</a> </nav> </div> </div> <br>
