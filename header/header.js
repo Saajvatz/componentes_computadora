cd174f3780a8217bc6f4f95c0bdd4cee2a9d6aa2
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-16 10:03:24 -0600

    lateral

--- header/header.js
+++ header/header.js
@@ -4,7 +4,7 @@ let div_header = document.querySelector(".header")
 
 div_header.innerHTML = `
 
-<div class="Cuadro"> <div class="Palabra"> <nav> <a href="Servicios.html">Servicios🦾</a></nav></div></div> 
+
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="index.html">inicio </a></nav></div></div> 
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="Ajustes.html">Ajustes🔧</a></nav></div></div> 
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="Menu_Principal.html">Menu ※</a></nav></div></div>`;
