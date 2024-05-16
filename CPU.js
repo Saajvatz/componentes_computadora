90cf2ae7d8d6cb81b0c1e3d1e98a76b14a10d108
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-15 18:02:16 -0600

    todos los html

---
+++ CPU.js
@@ -0,0 +1,11 @@
+let div_header = document.querySelector(".header")
+
+
+div_header.innerHTML = `
+<h1>La unidad central de procesamiento (conocida por las siglas CPU, del inglés Central Processing Unit) o procesador es un componente del hardware dentro de un ordenador, teléfonos inteligentes, y otros dispositivos programables.
+
+    Su función es interpretar las instrucciones de un programa informático mediante la realización de las operaciones básicas aritméticas, lógicas, y externas (procedentes de la unidad de entrada/salida). Su diseño y su avance ha variado notablemente desde su creación, aumentando su eficiencia y potencia y reduciendo el consumo de energía y el coste.
+    
+    Un ordenador puede contener más de una CPU (multiprocesamiento). En la actualidad, los microprocesadores están constituidos por un único circuito integrado (chip) aunque existen los procesadores multinúcleo (varias CPU en un solo circuito integrado). Un circuito integrado que contiene una CPU también puede contener los dispositivos periféricos y otros componentes de un sistema informático; similar a un microcontrolador (menos potente en RAM) se le denomina sistema en un chip (SoC).</h1>
+
+    <img src="https://th.bing.com/th/id/OIP.lU8fmxIMo6JCsaGVSEIT9wHaGS?w=208&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="">`;
