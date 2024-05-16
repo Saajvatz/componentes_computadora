4441b8c1e924aee55e0292f5dad59aa7764c8f1f
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-15 18:49:13 -0600

    SIM

--- CPU.js
+++ CPU.js
@@ -2,10 +2,10 @@ let div_footer = document.querySelector(".footer")
 
 
 div_footer.innerHTML = `
-<h1>La unidad central de procesamiento (conocida por las siglas CPU, del inglés Central Processing Unit) o procesador es un componente del hardware dentro de un ordenador, teléfonos inteligentes, y otros dispositivos programables.
+<h3>La unidad central de procesamiento (conocida por las siglas CPU, del inglés Central Processing Unit) o procesador es un componente del hardware dentro de un ordenador, teléfonos inteligentes, y otros dispositivos programables.
 
     Su función es interpretar las instrucciones de un programa informático mediante la realización de las operaciones básicas aritméticas, lógicas, y externas (procedentes de la unidad de entrada/salida). Su diseño y su avance ha variado notablemente desde su creación, aumentando su eficiencia y potencia y reduciendo el consumo de energía y el coste.
     
-    Un ordenador puede contener más de una CPU (multiprocesamiento). En la actualidad, los microprocesadores están constituidos por un único circuito integrado (chip) aunque existen los procesadores multinúcleo (varias CPU en un solo circuito integrado). Un circuito integrado que contiene una CPU también puede contener los dispositivos periféricos y otros componentes de un sistema informático; similar a un microcontrolador (menos potente en RAM) se le denomina sistema en un chip (SoC).</h1>
+    Un ordenador puede contener más de una CPU (multiprocesamiento). En la actualidad, los microprocesadores están constituidos por un único circuito integrado (chip) aunque existen los procesadores multinúcleo (varias CPU en un solo circuito integrado). Un circuito integrado que contiene una CPU también puede contener los dispositivos periféricos y otros componentes de un sistema informático; similar a un microcontrolador (menos potente en RAM) se le denomina sistema en un chip (SoC).</h3>
 
     <img src="https://th.bing.com/th/id/OIP.lU8fmxIMo6JCsaGVSEIT9wHaGS?w=208&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="">`;
---
+++ GPU.js
@@ -0,0 +1,6 @@
+let div_footer = document.querySelector(".footer")
+
+
+div_footer.innerHTML = ` <h2>GPU es el acrónimo en inglés de “Unidad de Procesamiento de Gráficos”. En términos generales, es un tipo de procesador que maneja y acelera la representación de gráficos (la generación de las imágenes que verá en las pantallas de las computadoras). Esto es especialmente importante para tareas informáticas como renderizado o modelado 3D y juegos de cierta categoría.</h2>
+
+<img src="https://techterms.com/img/lg/gpu_166.jpg" alt="">`;
