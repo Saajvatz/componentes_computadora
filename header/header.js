5b23686fdc98b4de26f17d4e46c857b9f079c5b2
pagiron <pagiron@scl.edu.gt>
2024-05-16 10:05:54 -0600

    Zuuz

--- header/header.js
+++ header/header.js
@@ -4,13 +4,8 @@ let div_header = document.querySelector(".header")
 
 div_header.innerHTML = `
 
-<<<<<<< HEAD
 
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="index.html">inicio </a></nav></div></div> 
-=======
-<div class="Cuadro"> <div class="Palabra"> <nav> <a href="Servicios.html">Servicios🦾</a></nav></div></div> 
-<div class="Cuadro"> <div class="Palabra"> <nav> <a href="index.html">Inicio </a></nav></div></div> 
->>>>>>> 04c2ea8f45926530ea6771a574c690754f421964
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="Ajustes.html">Ajustes🔧</a></nav></div></div> 
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="Menu_Principal.html">Menu ※</a></nav></div></div>`;
 
