1b323b451ee8dcfa5c8b9fec44125752f47bafa7
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-16 11:40:10 -0600

    ayuda

--- header/header.js
+++ header/header.js
@@ -4,7 +4,7 @@ let div_header = document.querySelector(".header")
 
 div_header.innerHTML = `
 
-
+<img src="https://th.bing.com/th/id/OIP.5RACxP1geYDAX5EB3UFPhQAAAA?rs=1&pid=ImgDetMain" alt="">
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="index.html">Inicio 🚩 </a></nav></div></div> 
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="Ajustes.html">Ayuda🔧</a></nav></div></div> 
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="https://github.com/Saajvatz/componentes_computadora">Repository</a></nav></div></div>`;
