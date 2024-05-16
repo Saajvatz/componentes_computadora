2a0372fc0bd9438e0bcdb08ea8cd96fde62e7f2f
pagiron <pagiron@scl.edu.gt>
2024-05-15 18:49:38 -0600

    Todo esta hecho

---
+++ GPU.js
@@ -0,0 +1,6 @@
+let div_footer = document.querySelector(".footer")
+
+div_footer.innerHTML = `
+
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fconcepto.de%2Fwp-content%2Fuploads%2F2014%2F08%2FCPU-e1551228076500.jpg&tbnid=n5q5IcsFjptiJM&vet=12ahUKEwjynajF-ZCGAxWfdDABHd-xBi8QMygDegQIARB4..i&imgrefurl=https%3A%2F%2Fconcepto.de%2Fcpu%2F&docid=58BlI-JO0Eix5M&w=800&h=400&q=imagenes%20de%20cpu&hl=es&ved=2ahUKEwjynajF-ZCGAxWfdDABHd-xBi8QMygDegQIARB4" alt="">
+<h1>: El CPU, o Unidad Central de Procesamiento, es el componente principal de un ordenador encargado de interpretar y ejecutar la mayoría de las instrucciones provenientes del hardware y software. A menudo se le conoce como el "cerebro" del ordenador. </h1>`;
---
+++ SSD.js
@@ -0,0 +1,6 @@
+let div_footer = document.querySelector(".footer")
+
+div_footer.innerHTML = `
+
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.pccomponentes.com%2Fpcblog%2F1423%2Fshutterstock-1901608936-opt.jpg&tbnid=Z1HBMifmCxuLnM&vet=12ahUKEwjZ78DP95CGAxWsFmIAHZ_vD-gQMygQegUIARCVAQ..i&imgrefurl=https%3A%2F%2Fwww.pccomponentes.com%2Fpor-que-instalar-un-ssd-en-tu-portatil&docid=xYY2hdr6auRCHM&w=1200&h=900&q=SSD&hl=es&ved=2ahUKEwjZ78DP95CGAxWsFmIAHZ_vD-gQMygQegUIARCVAQ" alt="">
+<h1> El SSD (Solid State Drive) es un dispositivo de almacenamiento que utiliza memoria flash para almacenar datos de manera persistente. A diferencia de los discos duros tradicionales (HDD), los SSD no tienen partes móviles, lo que les permite ser más rápidos y duraderos.</h1>`;
