763f6434d3b573fee6043ec02e934c46610fbe6c
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-16 09:52:53 -0600

    incio

--- header/header.js
+++ header/header.js
@@ -5,7 +5,7 @@ let div_header = document.querySelector(".header")
 div_header.innerHTML = `
 
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="Servicios.html">Servicios🦾</a></nav></div></div> 
-<div class="Cuadro"> <div class="Palabra"> <nav> <a href="Contactos.html">Contactos 👤 </a></nav></div></div> 
+<div class="Cuadro"> <div class="Palabra"> <nav> <a href="index.html">inicio </a></nav></div></div> 
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="Ajustes.html">Ajustes🔧</a></nav></div></div> 
-<div class="Cuadro"> <div class="Palabra"> <nav> <a href="Menu_Principal.html">Menu🏯</a></nav></div></div>`;
+<div class="Cuadro"> <div class="Palabra"> <nav> <a href="Menu_Principal.html">Menu ※</a></nav></div></div>`;
 
