1a7f1e92f7d4a706d5444db7d1fb2f659cd36822
pagiron <pagiron@scl.edu.gt>
2024-05-16 12:17:05 -0600

    Version101

--- footer/footer.js
+++ footer/footer.js
@@ -2,5 +2,5 @@ let div_footer=document.querySelector(".footer")
 
 div_footer.innerHTML = `
 <p>" Los componentes de una computadora se divide en dos partes una como “hardware” que hacen referencia aquellos elementos físicos de una computadora o sistema informático, aquellos componentes internos que lo conforman. Básicamente son las partes tangibles de la computadora."</p>
-<img src="https://th.bing.com/th/id/OIP.l_ZV0FftOi-4O6YeIpiXawHaHa?pid=ImgDet&w=179&h=179&c=7&dpr=1%2C5" alt="">
+<img src="https://th.bing.com/th/id/R.31986dd32934f4cb5110b80e1232959e?rik=39Yj0tpEnAgfSA&riu=http%3a%2f%2fmemeschistosos.net%2fwp-content%2fuploads%2f2016%2f08%2fmemes-de-guapos-perrito.jpg&ehk=CcjImfQqrpELGqpxtkcNGQWifRwXYbvEonmPtlbwZwM%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1" alt="">
 <img src="https://gcdn.emol.cl/humor/files/2016/11/memes-de-perros-1.jpg" alt="">`;
