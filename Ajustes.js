3b34d8100de400b020ef60d78be60f1d17d4bf35
pagiron <pagiron@scl.edu.gt>
2024-05-16 11:45:02 -0600

    Ayuda terminado

--- Ajustes.js
+++ Ajustes.js
@@ -2,7 +2,8 @@ let div_footer=document.querySelector(".footer")
 
 div_footer.innerHTML = `
 <h4>si tiene algina duda comunicarse con </h4>
-<div class="Cuadro"> <div class="Palabra"> <nav> <a href="https://www.instagram.com/pabl_oandres/?utm_source=ig_web_button_share_sheet">Pagirony</a></nav></div></div>
-<div class="Cuadro"> <div class="Palabra"> <nav> <a href="">Saajvatz</a></nav></div></div>`;`;
-`;
+<div class="Cuadro"> <div class="Palabra"> <nav> <a href="https://www.instagram.com/pabl_oandres/?utm_source=ig_web_button_share_sheet">Pagiron</a></nav></div></div>
+
+<div class="Cuadro"> <div class="Palabra"> <nav> <a href="https://www.instagram.com/thesaajvatz?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">Saajvatz</a></nav></div></div>`;
+
 
