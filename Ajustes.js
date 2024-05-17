82291e915403979c834e6a92a3277898c644f401
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-16 19:25:13 -0600

    r

--- Ajustes.js
+++ Ajustes.js
@@ -4,6 +4,15 @@ div_footer.innerHTML = `
 <h4>si tiene algina duda comunicarse con </h4>
 <div class="Cuadro"> <div class="Palabra"> <nav> <a href="https://www.instagram.com/pabl_oandres/?utm_source=ig_web_button_share_sheet">Pagiron</a></nav></div></div>
 
-<div class="Cuadro"> <div class="Palabra"> <nav> <a href="https://www.instagram.com/thesaajvatz?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">Saajvatz</a></nav></div></div>`;
+<div class="Cuadro"> <div class="Palabra"> <nav> <a href="https://www.instagram.com/thesaajvatz?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">Saajvatz</a></nav></div></div>
+
+
+<h4>Perfiles github</h4>
+
+<div class="Cuadro"> <div class="Palabra"> <nav> <a href="https://github.com/RG4Pablo">Pagiron</a></nav></div></div>
+<div class="Cuadro"> <div class="Palabra"> <nav> <a href="https://github.com/Saajvatz">Saajvatz</a></nav></div></div>
+
+`;
+
 
 
