652a942c6059a98aa5533b5aca19930585f8abeb
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-16 20:06:56 -0600

    footer.js

--- footer/footer.js
+++ footer/footer.js
@@ -3,6 +3,7 @@ let div_footer=document.querySelector(".footer")
 div_footer.innerHTML = `
 <p>" Los componentes de una computadora se divide en dos partes una como “hardware” que hacen referencia aquellos elementos físicos de una computadora o sistema informático, aquellos componentes internos que lo conforman. Básicamente son las partes tangibles de la computadora."</p>
 <img src="https://th.bing.com/th/id/R.31986dd32934f4cb5110b80e1232959e?rik=39Yj0tpEnAgfSA&riu=http%3a%2f%2fmemeschistosos.net%2fwp-content%2fuploads%2f2016%2f08%2fmemes-de-guapos-perrito.jpg&ehk=CcjImfQqrpELGqpxtkcNGQWifRwXYbvEonmPtlbwZwM%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1" alt="">
-<img src="https://www.bing.com/ck/a?!&&p=d250983779a3a5dbJmltdHM9MTcxNTgxNzYwMCZpZ3VpZD0zMGZiYWVhMy1iZWIwLTZlMGQtMjc0NC1iZDRkYmYyMDZmYzMmaW5zaWQ9NTcyMw&ptn=3&ver=2&hsh=3&fclid=30fbaea3-beb0-6e0d-2744-bd4dbf206fc3&u=a1L2ltYWdlcy9zZWFyY2g_cT1pbWFnZW5lcyUyMGRlJTIwbWVtZXMlMjBkZSUyMHBlcnJvcyUyMGZpbm9zJkZPUk09SVFGUkJBJmlkPTg3Njg3RjU2NTRBRTA3QjE5OUVGM0ZBNUM4NzgxMTk4OUQ0QUY0NDk&ntb=1" alt="">´;
+
+<img src="https://th.bing.com/th/id/OIP.mA0u-p6tDBRZkIvb1mLTGQHaHa?pid=ImgDet&w=179&h=179&c=7&dpr=1,5"alt="">`;
 
 
